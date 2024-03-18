56fe8f2a6bda4a155560d2b5df9937c6380f255d
Piotr <P.Larkowski90@gmail.com>
2024-03-18 11:57:48 +0100

    Add basic operations for calculator

--- src/CalculatorConundrum.java
+++ src/CalculatorConundrum.java
@@ -1,5 +1,16 @@
 public class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
+        int value = 0;
+        if(operation.equals("+")){
+            value = operand1+operand2;
+            return "" +operand1+" + " + operand2 + " = " + value;
+        }else if(operation.equals("*")){
+            value = operand1*operand2;
+            return "" +operand1+" * " + operand2 + " = " + value;
+        }else if(operation.equals("/")){
+            value = operand1/operand2;
+            return "" +operand1+" / " + operand2 + " = " + value;
+        }
         return "0";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        CalculatorConundrum calculatorConundrum = new CalculatorConundrum();
+        System.out.println("Add values: " + calculatorConundrum.calculate(+ 5, 5, "+"));
+        System.out.println("Multiply values: " + calculatorConundrum.calculate(+ 5, 5, "*"));
+        System.out.println("Divided values: " + calculatorConundrum.calculate(+ 5, 5, "/"));
     }
 }
